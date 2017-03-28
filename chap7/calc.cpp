616d8ab0e0af31e78790ba2845524d0afabe7df2
nakao <kimitsugu.nakao@gmail.com>
2017-03-28 21:59:20 +0900

    7.4 負の数字 追加

--- chap7/calc.cpp
+++ chap7/calc.cpp
@@ -109,14 +109,18 @@ double primary()
     Token t = ts.get();
     switch (t.kind) {
     case '(':    // handle '(' expression ')'
-        {    
-            double d = expression();
-            t = ts.get();
-            if (t.kind != ')') error("')' expected");
-            return d;
-        }
+    {    
+        double d = expression();
+        t = ts.get();
+        if (t.kind != ')') error("')' expected");
+        return d;
+    }
     case '8':            // we use '8' to represent a number
         return t.value;  // return the number's value
+    case '-':
+        return - primary();
+    case '+':
+        return + primary();
     default:
         error("primary expected");
     }
